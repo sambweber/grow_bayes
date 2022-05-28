4134f7c6c9af79bad4f8829491b365c07af49b77
sambweber <42612830+sambweber@users.noreply.github.com>
2022-05-28 19:13:29 +0100

    Update grow_bayes.R

--- R/grow_bayes.R
+++ R/grow_bayes.R
@@ -289,7 +289,7 @@ DIC = function(model){
 MPSRF = function(model){
   
   if(!inherits(model,'rjags')) stop("model should be an object of class rjags")
-  coda::gelman.diag(as.mcmc(x))$mpsrf
+  coda::gelman.diag(as.mcmc(model))$mpsrf
 
 }
 
